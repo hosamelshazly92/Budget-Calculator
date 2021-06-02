68456f6852d840306b093218088753430de622b7
Hosam Elshazly <hosamelshazly92@gmail.com>
2021-06-02 17:35:14 +0200

    define alert, form, list & item components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
+import Alert from './components/Alert';
 import './App.css';
 
 function App() {
-  return (
-    <div>Test</div>
-  );
+  return <>
+    <Alert></Alert>
+    <ExpenseForm />
+    <ExpenseList />
+  </>;
 }
 
 export default App;
