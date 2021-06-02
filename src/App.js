b08a1c32911ee8d0c196b0d34e506ddc93c931eb
Hosam Elshazly <hosamelshazly92@gmail.com>
2021-06-02 18:34:17 +0200

    define list & item function components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
 import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
+const initialExpenses = [
+  {
+    id: uuidv4(),
+    charge: "rent",
+    amount: 1600
+  },
+  {
+    id: uuidv4(),
+    charge: "car payment",
+    amount: 400
+  },
+  {
+    id: uuidv4(),
+    charge: "credit card bill",
+    amount: 1200
+  }
+];
+
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return <>
-    <Alert></Alert>
-    <ExpenseForm />
-    <ExpenseList />
+    <Alert />
+    <h1>Budget Calculator</h1>
+    <main className="App">
+      <ExpenseForm />
+      <ExpenseList expenses={expenses} />
+    </main>
+    <h1>
+      total spending: <span className="total">$ { expenses.reduce((acc, cur) => { return acc += cur.amount}, 0) }</span>
+    </h1>
   </>;
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { MdDelete, MdEdit } from 'react-icons/md';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ expense }) => {
+    const { id, charge, amount } = expense;
+
     return (
-        <div>
-            hello from expense item
-        </div>
+        <li className="item">
+            <div className="info">
+                <span className="expense">{ charge }</span>
+                <span className="amount">${ amount }</span>
+            </div>
+            <div>
+                <button className="edit-btn" aria-labe="edit button">
+                    <MdEdit />
+                </button>
+                <button className="clear-btn" aria-labe="delete button">
+                    <MdDelete />
+                </button>
+            </div>
+        </li>
     )
 }
 
